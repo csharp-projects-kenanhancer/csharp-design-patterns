b8281685f5e7a8d415a3b0701f19ab96c44ace18
kenan <kenan@10dot1.co.uk>
2023-09-29 03:12:51 +0100

    First commit

---
+++ example1/Actor.cs
@@ -0,0 +1,95 @@
+namespace example1;
+
+public class Actor
+{
+    public Guid Id { get; set; }
+    
+    // Personal Details
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string Biography { get; private set; }
+    
+    // Social Media Links
+    public string? Twitter { get; private set; }
+    public string? Instagram { get; private set; }
+    public string? Facebook { get; private set; }
+    public string? OfficialWebsite { get; private set; }
+
+    public Actor(string firstName, string lastName, string biography, string? twitter, string? instagram, string? facebook, string? officialWebsite)
+    {
+        Id = Guid.NewGuid();
+        FirstName = firstName;
+        LastName = lastName;
+        Biography = biography;
+        Twitter = twitter;
+        Instagram = instagram;
+        Facebook = facebook;
+        OfficialWebsite = officialWebsite;
+    }
+
+    public void UpdateTwitterLink(string newLink)
+    {
+        if (!IsValidSocialMediaLink(newLink, "twitter.com"))
+            throw new InvalidOperationException("Invalid Twitter link.");
+
+        Twitter = newLink;
+    }
+
+    public void UpdateInstagramLink(string newLink)
+    {
+        if (!IsValidSocialMediaLink(newLink, "instagram.com"))
+            throw new InvalidOperationException("Invalid Instagram link.");
+
+        Instagram = newLink;
+    }
+
+    public void UpdateFacebookLink(string newLink)
+    {
+        if (!IsValidSocialMediaLink(newLink, "facebook.com"))
+            throw new InvalidOperationException("Invalid Facebook link.");
+
+        Facebook = newLink;
+    }
+
+    public void UpdateOfficialWebsite(string newLink)
+    {
+        if (!IsValidSocialMediaLink(newLink))
+            throw new InvalidOperationException("Invalid official website link.");
+
+        OfficialWebsite = newLink;
+    }
+
+    public void RemoveTwitterLink()
+    {
+        Twitter = null;
+    }
+
+    public void RemoveInstagramLink()
+    {
+        Instagram = null;
+    }
+
+    public void RemoveFacebookLink()
+    {
+        Facebook = null;
+    }
+
+    public void RemoveOfficialWebsite()
+    {
+        OfficialWebsite = null;
+    }
+
+    private bool IsValidSocialMediaLink(string link, string? expectedDomain = null)
+    {
+        if (string.IsNullOrWhiteSpace(link))
+            return false;
+
+        if (!Uri.TryCreate(link, UriKind.Absolute, out Uri? uri))
+            return false;
+
+        if (expectedDomain != null && !uri.Host.EndsWith(expectedDomain, StringComparison.OrdinalIgnoreCase))
+            return false;
+        
+        return true;
+    }
+}
---
+++ example1/Program.cs
@@ -0,0 +1,29 @@
+﻿using example1;
+
+var actor = new Actor("John", "Doe", "John Doe is an acclaimed actor...", "https://twitter.com/johndoe",
+    "https://instagram.com/johndoe",
+    "https://facebook.com/johndoe", "https://www.johndoe.com");
+
+// Get a specific social media link
+Console.WriteLine($"Original Twitter: {actor.Twitter}");
+Console.WriteLine($"Original Instagram: {actor.Instagram}");
+Console.WriteLine($"Original Facebook: {actor.Facebook}");
+Console.WriteLine($"Original OfficialWebsite: {actor.OfficialWebsite}");
+    
+// Update social media links for the actor
+actor.UpdateTwitterLink("https://twitter.com/new-johndoe");
+actor.UpdateInstagramLink("https://instagram.com/new-johndoe");
+actor.UpdateFacebookLink("https://facebook.com/new-johndoe");
+actor.UpdateOfficialWebsite("https://www.new-johndoe.com");
+
+// Get a specific social media link
+Console.WriteLine($"Updated Twitter: {actor.Twitter}");
+Console.WriteLine($"Updated Instagram: {actor.Instagram}");
+Console.WriteLine($"Updated Facebook: {actor.Facebook}");
+Console.WriteLine($"Updated OfficialWebsite: {actor.OfficialWebsite}");
+
+// Remove a specific social media link
+actor.RemoveTwitterLink();
+actor.RemoveInstagramLink();
+actor.RemoveFacebookLink();
+actor.RemoveOfficialWebsite();
